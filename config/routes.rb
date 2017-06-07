f15f5931729bd4593c774e57a2ba46d8213a478e
Duera <LMWoodfield@hotmail.com>
2017-06-07 19:21:35 +0100

    Finished adding index, edit, new, delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'portfolio_items/new'
+  root 'static#home'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #resources :portfolio, controller: :portfolio_items
+  resources :portfolio_items, :path => "portfolio"
+
 end
